d23caf7275edd2f647bb726eea66e52603a06481
netstermonir <monirhossain6163@gmail.com>
2024-09-21 22:15:37 +0600

    f

--- src/app/(withLayout)/page.tsx
+++ src/app/(withLayout)/page.tsx
@@ -1,20 +1,6 @@
 import HomeContent from "@/components/home/HomeContent";
 
 export default function Home() {
-  if (
-    navigator.userAgent.includes("FBAN") ||
-    navigator.userAgent.includes("FBAV")
-  ) {
-    return (
-      <div>
-        <h1>Facebook App Browser Detected</h1>
-        <p>
-          Please open this link in your default browser to view the content.
-        </p>
-      </div>
-    );
-  }
-
   return (
     <div>
       <HomeContent />
