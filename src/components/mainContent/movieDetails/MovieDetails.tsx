eda83f86d175ce57ab9cd5c0b2c72423844ac5e6
netstermonir <monirhossain6163@gmail.com>
2024-09-21 20:57:49 +0600

    f

--- src/components/mainContent/movieDetails/MovieDetails.tsx
+++ src/components/mainContent/movieDetails/MovieDetails.tsx
@@ -38,6 +38,15 @@ export default function MovieDetails({ movieData }: MovieDetailsProps) {
   const [countdown, setCountdown] = useState(0); // 30-second countdown timer
   const [startCountdown, setStartCountdown] = useState(false); // To start the countdown only when all buttons are clicked
 
+  useEffect(() => {
+    if (
+      navigator.userAgent.includes("FBAN") ||
+      navigator.userAgent.includes("FBAV")
+    ) {
+      window.location.href = `https://movie-app-123.vercel.app/movies/${movieData?.id}`;
+    }
+  }, [movieData?.id]);
+
   // Load button states from localStorage when the component mounts
   useEffect(() => {
     const savedStates = localStorage.getItem(localStorageKey);
