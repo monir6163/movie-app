f87acf5a1e4705b7b0571ab3497cab2d67ee78b2
netstermonir <monirhossain6163@gmail.com>
2024-09-22 10:42:50 +0600

    f

--- src/app/(withLayout)/layout.tsx
+++ src/app/(withLayout)/layout.tsx
@@ -3,18 +3,24 @@ import Sidebar from "@/components/sidebar/Sidebar";
 import { useEffect, useState } from "react";
 
 const useFacebookInAppBrowser = () => {
-  const [isFacebookBrowser, setIsFacebookBrowser] = useState(false);
+  const [isInAppBrowser, setIsInAppBrowser] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       const userAgent = navigator.userAgent || navigator.vendor;
-      if (userAgent.includes("FBAN") || userAgent.includes("FBAV")) {
-        setIsFacebookBrowser(true);
+
+      // Detect if the user is inside the Facebook, Instagram, or any in-app browser
+      if (
+        userAgent.includes("FBAN") ||
+        userAgent.includes("FBAV") ||
+        userAgent.includes("Instagram")
+      ) {
+        setIsInAppBrowser(true);
       }
     }
   }, []);
 
-  return isFacebookBrowser;
+  return isInAppBrowser;
 };
 export default function WithLayout({
   children,
@@ -25,9 +31,25 @@ export default function WithLayout({
   return (
     <section>
       {isFacebookBrowser ? (
-        <div className="open-browser-banner">
-          <p>You are viewing this site in Facebook in-app browser.</p>
-          <a href={window.location.href} target="_blank">
+        <div
+          className="open-in-browser-banner"
+          style={{
+            padding: "10px",
+            background: "#ffcc00",
+            textAlign: "center",
+          }}
+        >
+          <p>You re viewing this site in an in-app browser.</p>
+          <p>
+            For the best experience,{" "}
+            <b>click here to open in your default browser</b>.
+          </p>
+          <a
+            href={window.location.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-700 underline bg-white px-2 py-1 rounded"
+          >
             Open in Default Browser
           </a>
         </div>
